703b72cc4ce237179ceb8ee2cf87139972372514
Nandi <huszka.nandor@gmail.com>
2015-12-09 08:53:59 +0100

    Compute average, minimum, maximum in evaluation result. Initial check for inputs.

--- src/com/nhuszka/collection_evaluator/result/CollectionEvaluationResult.java
+++ src/com/nhuszka/collection_evaluator/result/CollectionEvaluationResult.java
@@ -8,7 +8,18 @@ public class CollectionEvaluationResult {
 	private final List<EvaluationDetails> evaluationResults = new ArrayList<>();
 
 	public final void addEvaluationResults(String evaluationDescription, List<Long> elapsedNanoSeconds) {
+		checkCorrectEvaluationResult(evaluationDescription, elapsedNanoSeconds);
+		
 		evaluationResults.add(new EvaluationDetails(evaluationDescription, elapsedNanoSeconds));
+	}
+
+	private void checkCorrectEvaluationResult(String evaluationDescription, List<Long> elapsedNanoSeconds) {
+		if (evaluationDescription == null || evaluationDescription.isEmpty()) {
+			// TODO exception to indicate something is wrong with evaluation
+		}
+		if (elapsedNanoSeconds == null || elapsedNanoSeconds.isEmpty()) {
+			// TODO exception to indicate something is wrong with evaluation
+		}
 	}
 
 	public void processResults() {
@@ -21,15 +32,54 @@ public class CollectionEvaluationResult {
 		
 		private final String evaluationDescription;
 		private final List<Long> measurements;
+		private Long minimum;
+		private Long maximum;
+		private Long average;
 		
 		public EvaluationDetails(String evaluationDescription, List<Long> measurements) {
 			this.evaluationDescription = evaluationDescription;
 			this.measurements = measurements;
+			calculateMetrics();
+		}
+		
+		private void calculateMetrics() {
+			Long sum = 0L;
+			for (Long measurement : measurements) {
+				sum += measurement;
+				minimum = reCalculateMinimum(measurement);
+				maximum = reCalculateMaximum(measurement);
+			}
+			average = sum / measurements.size();
+		}
+		
+		private Long reCalculateMinimum(Long measurement) {
+			if (minimum == null) {
+				return measurement;
+			}
+			return measurement < minimum 
+					? measurement 
+					: minimum;
+		}
+		
+		private Long reCalculateMaximum(Long measurement) {
+			if (maximum == null) {
+				return measurement;
+			}
+			return measurement > maximum
+					? measurement 
+					: maximum;
 		}
 		
 		@Override
 		public String toString() {
-			return evaluationDescription + ": " + measurements;
+			StringBuilder evaluation = new StringBuilder();
+			evaluation.append(evaluationDescription)
+				.append(":")
+				.append(" minimum: " + minimum)
+				.append(" maximum: " + maximum)
+				.append(" average: " + average);
+			
+			return evaluation.toString();
 		}
 	}
 }
